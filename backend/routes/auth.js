996355f5522057c188bbd26cdf7ffa7ec6d32987
Hemant Singh Bhadoria <hemantsinghbhadoriya1@gmail.com>
2023-01-29 16:18:50 +0530

    bcrypt, has & authtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,32 +2,67 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+const JWT_SECRET = "HSBisagoodbb$oy";
 
-//Create a User using: POST "/api/auth". Dosn't require auth
+//Create a User using: POST "/api/auth/createuser". No login require
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter Valid Name").isLength({ min: 3 }),
     body("email", "Enter Valid Email").isEmail(),
-    body("password", "Password must be atleast 5 character").isLength({
+    body("password", "password must be atleast 5 character").isLength({
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If there are error, return Bad request and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {console.log(err)
-    res.json({ error: "Please enter a unique value for email", message:err.message})
-  })}
+    // check wheather the user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      // create a new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      //   .then((user) => res.json(user))
+      //   .catch((err) => {
+      //     console.log(err);
+      //     res.json({
+      //       error: "Please enter a unique value for email",
+      //       message: err.message,
+      //     });
+      //   });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      return res.json({authtoken});
+    } catch (error) {
+      console.error(error.message);
+    }
+    return res.status(500).json("Some error occured");
+  }
 );
 
 module.exports = router;
